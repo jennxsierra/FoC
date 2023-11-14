6322b35be9ebf29cd0000a1b1d9cf9d8c162510a
Jennessa Sierra <143227481+jennxsierra@users.noreply.github.com>
2023-11-14 14:36:47 -0600

    Added about me and 2 projects

---
+++ scripts/flip-flop.js
@@ -0,0 +1,41 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  const x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+let flipFlopState = "off";
+
+function toggleSInput() {
+  if (flipFlopState === "off") {
+    flipFlopState = "sq";
+  } else if (flipFlopState === "sq") {
+    flipFlopState = "q";
+  } else if (flipFlopState === "q") {
+    flipFlopState = "sq";
+  } else if (flipFlopState === "sr" || flipFlopState === "r") {
+    flipFlopState = "sr";
+  }
+  updateFlipFlopImage();
+}
+
+function toggleRInput() {
+  if (flipFlopState === "off") {
+    flipFlopState = "r";
+  } else if (flipFlopState === "sq") {
+    flipFlopState = "sr";
+  } else if (flipFlopState === "sr") {
+    flipFlopState = "sq";
+  }
+  updateFlipFlopImage();
+}
+
+function updateFlipFlopImage() {
+  document.getElementById(
+    "flipFlopImage",
+  ).src = `../../images/flip-flop-${flipFlopState}.png`;
+}
---
+++ scripts/half-adder.js
@@ -0,0 +1,41 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  const x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+let aInputPressed = false;
+let bInputPressed = false;
+let abInputPressed = false;
+
+function toggleAInput() {
+  if (!aInputPressed) {
+    document.getElementById("halfAdderImage").src = "../../images/ha-a-on.png";
+    aInputPressed = true;
+    bInputPressed = false;
+    abInputPressed = false;
+  }
+}
+
+function toggleBInput() {
+  if (!bInputPressed) {
+    document.getElementById("halfAdderImage").src = "../../images/ha-b-on.png";
+    aInputPressed = false;
+    bInputPressed = true;
+    abInputPressed = false;
+  }
+}
+
+function toggleABInput() {
+  if (!abInputPressed) {
+    document.getElementById("halfAdderImage").src = "../../images/ha-ab-on.png";
+    aInputPressed = false;
+    bInputPressed = false;
+    abInputPressed = true;
+  }
+}
+
