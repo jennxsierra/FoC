a4d20d3d5a7df7917577b976e3340240a312c084
Jennessa Sierra <143227481+jennxsierra@users.noreply.github.com>
2023-11-20 01:35:06 -0600

    misc: corrected binarytohexadecimal.js

--- scripts/binarytohexadecimal.js
+++ scripts/binarytohexadecimal.js
@@ -12,12 +12,22 @@ function convertBinaryToHex() {
   const binaryInput = document.getElementById("binaryInput").value;
   const decimalValue = parseInt(binaryInput, 2);
   const hexValue = decimalValue.toString(16).toUpperCase();
-  document.getElementById("hexOutput").innerText = `Result: ${hexValue}`;
+  document.getElementById("hexOutput").innerText = `Hexadecimal: ${hexValue}`;
 }
 
 function convertHexToBinary() {
   const hexInput = document.getElementById("hexInput").value;
-  const decimalValue = parseInt(hexInput, 16);
-  const binaryValue = decimalValue.toString(2);
-  document.getElementById("binaryOutput").innerText = `Result: ${binaryValue}`;
+
+  // Ensure that the input is a valid hexadecimal value
+  if (/^[0-9A-Fa-f]+$/.test(hexInput)) {
+    const binaryValue = parseInt(hexInput, 16)
+      .toString(2)
+      .padStart(hexInput.length * 4, "0");
+    document.getElementById(
+      "binaryOutput",
+    ).innerText = `Binary: ${binaryValue}`;
+  } else {
+    document.getElementById("binaryOutput").innerText =
+      "Please enter a valid hexadecimal value.";
+  }
 }
